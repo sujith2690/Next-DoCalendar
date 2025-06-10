59f65d0e5abf13674c5020a4022d851c0eca69a4
Sujith P <sujithp2690@gmail.com>
2025-06-10 18:23:22 +0530

    user profile error fixed

--- src/app/(calendar)/profile/[userId]/page.tsx
+++ src/app/(calendar)/profile/[userId]/page.tsx
@@ -1,19 +1,34 @@
-import CallButton from "@/app/(ui)/components/CallButton";
-import { notFound } from "next/navigation";
+// import CallButton from "@/app/(ui)/components/CallButton";
+// import { notFound } from "next/navigation";
 
 
-const UserProfile = async ({ params }: { params: { userId: string } }) => {
-    const { userId } = params;
-    if (parseInt(userId) > 10) {
-        notFound()
-    }
+// const UserProfile = async ({ params }: { params: { userId: string } }) => {
+//     const { userId } = params;
+//     if (parseInt(userId) > 10) {
+//         notFound()
+//     }
+//     return (
+//         <div>UserProfile -- {userId}
+//             <div>
+//                 <CallButton />
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default UserProfile;
+
+
+import CallButton from '@/app/(ui)/components/CallButton';
+import React from 'react'
+
+export default async function UserProfile({ params, }: { params: Promise<{ userId: string }>; }) {
+    const userId = (await params).userId
     return (
         <div>UserProfile -- {userId}
             <div>
                 <CallButton />
             </div>
         </div>
-    );
-};
-
-export default UserProfile;
+    )
+}
